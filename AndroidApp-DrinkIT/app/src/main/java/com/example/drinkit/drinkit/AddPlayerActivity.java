3d143d30ae22ee3b03e727b067f465d266e6a31b
Alice Olsson <alice_olsson@hotmail.com>
2018-09-25 13:50:12 +0200

    Designed addPlayer

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
@@ -1,14 +1,24 @@
 package com.example.drinkit.drinkit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class AddPlayerActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
+    }
+
+    public void nextToCategoryPage(View view) {
     }
 
     //emulator now works

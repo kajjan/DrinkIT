7fa3fa449d85e88849926977689de70e321d6a9c
Viktoria Enderstein <vikend@student.chalmers.se>
2018-09-25 14:13:53 +0200

    added first test in AddPlayerTest

---
+++ AndroidApp-DrinkIT/app/src/test/java/com/example/drinkit/drinkit/AddPlayerTest.java
@@ -0,0 +1,41 @@
+package com.example.drinkit.drinkit;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.DrinkIT;
+import Model.Player;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class AddPlayerTest {
+    @Test
+    public void addPlayer() {
+
+        String name="Viktoria";
+        boolean b = false;
+
+        List<Player> playersList=new ArrayList<>();
+
+        DrinkIT model= new DrinkIT(playersList, null,null);
+        model.addPlayer(name);
+
+        for(String s:model.getPlayersName()){
+            if (name.equals(s)){
+                b=true;
+            }
+        }
+
+        assert(b);
+    }
+
+
+
+}

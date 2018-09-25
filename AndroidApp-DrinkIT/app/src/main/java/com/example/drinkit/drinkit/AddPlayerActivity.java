f56bc1968f1b172ae370ea2c5e733dc8541afffc
Elin Eriksson <elinerikssonn@gmail.com>
2018-09-25 09:30:37 +0200

    made sure build and emulator works

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
@@ -10,4 +10,6 @@ public class AddPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
     }
+
+    //emulator now works 
 }

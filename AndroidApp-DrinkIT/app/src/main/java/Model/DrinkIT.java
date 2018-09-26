be34281931233d818dccb8616d746a8f1003dbb6
LisaFahlbeck <fahlisa@student.chalmers.se>
2018-09-26 17:38:41 +0200

    duration added buttons and short metod

--- AndroidApp-DrinkIT/app/src/main/java/Model/DrinkIT.java
+++ AndroidApp-DrinkIT/app/src/main/java/Model/DrinkIT.java
@@ -7,17 +7,25 @@ public class DrinkIT {
     private List<Player> players;
     private List<Card> cards;
     private List<Challenge> challenges;
+    private int duration;
 
-    public DrinkIT(List<Player> players, List<Card> cards, List<Challenge> challenges) {
+    public DrinkIT(List<Player> players, List<Card> cards, List<Challenge> challenges, int duration) {
         this.players = players;
         this.cards = cards;
         this.challenges = challenges;
+        this.duration = duration;
     }
 
     public void addPlayer(String name) {
         players.add(new Player(name));
     }
 
+    public void setDuration(List<Player> players, int duration){
+
+        duration = players.size()*duration;
+    }
+
+    //medtod för test
     public List<String> getPlayerNames() {
         List<String> names = new ArrayList<>();
         for (Player p : players) {
---
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/DurationActivity.java
@@ -0,0 +1,25 @@
+package com.example.drinkit.drinkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duration);
+    }
+
+    public void shortDuration(View view) {
+
+    }
+
+    public void mediumDuration(View view) {
+    }
+
+
+    public void longDuartion(View view) {
+    }
+}

e53dd6e959961f43b82786fb3a151a239733669c
Viktoria Enderstein <vikend@student.chalmers.se>
2018-09-28 09:19:09 +0200

    added so you can add 10 players instead of 3

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
@@ -19,7 +19,7 @@ public class AddPlayerActivity extends AppCompatActivity {
 
     Controller ctrl = new Controller();
 
-    EditText playerOne, playerTwo, playerThree;
+    EditText playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen;
     List<String> players = new ArrayList<>(10);
 
     @Override
@@ -39,10 +39,25 @@ public class AddPlayerActivity extends AppCompatActivity {
         playerOne = (EditText) findViewById(R.id.player1);
         playerTwo = (EditText) findViewById(R.id.player2);
         playerThree = (EditText) findViewById(R.id.player3);
+        playerFour = (EditText) findViewById(R.id.player4);
+        playerFive = (EditText) findViewById(R.id.player5);
+        playerSix = (EditText) findViewById(R.id.player6);
+        playerSeven = (EditText) findViewById(R.id.player7);
+        playerEight = (EditText) findViewById(R.id.player8);
+        playerNine= (EditText) findViewById(R.id.player9);
+        playerTen = (EditText) findViewById(R.id.player10);
 
         players.add(playerOne.getText().toString());
         players.add(playerTwo.getText().toString());
         players.add(playerThree.getText().toString());
+        players.add(playerFour.getText().toString());
+        players.add(playerFive.getText().toString());
+        players.add(playerSix.getText().toString());
+        players.add(playerSeven.getText().toString());
+        players.add(playerEight.getText().toString());
+        players.add(playerNine.getText().toString());
+        players.add(playerTen.getText().toString());
+
 
         for (int i = 0; i <players.size() ; i++) {
             if (players.get(i).isEmpty()){

f454ce82f379ede59f901244ee446c7abe0e68bd
Viktoria Enderstein <vikend@student.chalmers.se>
2018-09-28 13:11:27 +0200

    Kajsa and I created method completeListOfPlayers() and fixed problems in addPlayer and setDuration along the way. Now the method works but it's not connected yet.

--- AndroidApp-DrinkIT/app/src/main/java/Model/DrinkIT.java
+++ AndroidApp-DrinkIT/app/src/main/java/Model/DrinkIT.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DrinkIT {
-    private List<Player> players;
+    protected List<Player> players = new ArrayList<>();
     private List<Card> cards;
     private List<Challenge> challenges;
     private int durationOfGame;
+
 
     public DrinkIT(List<Player> players, List<Card> cards, List<Challenge> challenges, int durationOfGame) {
         this.players = players;
@@ -15,6 +16,10 @@ public class DrinkIT {
         this.challenges = challenges;
         this.durationOfGame = durationOfGame;
     }
+    public DrinkIT() {
+
+    }
+
 
 
     //Temporary constructor för duartion method
@@ -27,10 +32,13 @@ public class DrinkIT {
         players.add(new Player(name));
     }
 
+
+
     public void setDuration(List<Player> players, int duration){
-        //System.out.println("Knappen för vald tid är tryckt och antalet spelare multipliceras med "+ duration);
+        System.out.println("Knappen för vald tid är tryckt och antalet spelare multipliceras med "+ duration);
+
         durationOfGame = players.size()*duration;
-        completedPlayersList(players, durationOfGame);
+
     }
 
     public int getDurationOfGame() {
@@ -39,26 +47,32 @@ public class DrinkIT {
 
 
     //method to create a complete list with all the players multiplide with the duration time.
+    //Not connected to anything yet. But works!!!!
     public List<Player> completedPlayersList (List<Player> listOfPlayers, int durationOfGame){
         List<Player>completePlayerList = new ArrayList<>(durationOfGame);
-        int challengePerPlayer = durationOfGame/players.size();
+        int challengePerPlayer = durationOfGame/listOfPlayers.size();
         int i=0;
         for(Player player: listOfPlayers){
 
-            if(i!=challengePerPlayer) {
+            while(i!=challengePerPlayer) {
                 completePlayerList.add(player);
                 i++;
             }
-            else{
+            if(i==challengePerPlayer){
                 i=0;
             }
         }
         System.out.println(completePlayerList);
         return completePlayerList;
-
     }
 
 
+
+
+
+    public List<Player> getPlayers() {
+        return players;
+    }
 
 
 
--- AndroidApp-DrinkIT/app/src/main/java/Model/Player.java
+++ AndroidApp-DrinkIT/app/src/main/java/Model/Player.java
@@ -14,4 +14,6 @@ public class Player {
         return name;
     }
 
+
+
 }

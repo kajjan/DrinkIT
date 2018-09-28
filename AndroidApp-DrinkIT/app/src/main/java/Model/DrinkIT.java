9e077b8da286e9f793e299fef4ea210a65033201
Viktoria Enderstein <vikend@student.chalmers.se>
2018-09-28 10:29:49 +0200

    created method completeListOfPlayers()

--- AndroidApp-DrinkIT/app/src/main/java/Model/DrinkIT.java
+++ AndroidApp-DrinkIT/app/src/main/java/Model/DrinkIT.java
@@ -30,11 +30,40 @@ public class DrinkIT {
     public void setDuration(List<Player> players, int duration){
         //System.out.println("Knappen för vald tid är tryckt och antalet spelare multipliceras med "+ duration);
         durationOfGame = players.size()*duration;
+        completedPlayersList(players, durationOfGame);
     }
 
     public int getDurationOfGame() {
         return durationOfGame;
     }
+
+
+    //method to create a complete list with all the players multiplide with the duration time.
+    public List<Player> completedPlayersList (List<Player> listOfPlayers, int durationOfGame){
+        List<Player>completePlayerList = new ArrayList<>(durationOfGame);
+        int challengePerPlayer = durationOfGame/players.size();
+        int i=0;
+        for(Player player: listOfPlayers){
+
+            if(i!=challengePerPlayer) {
+                completePlayerList.add(player);
+                i++;
+            }
+            else{
+                i=0;
+            }
+        }
+        System.out.println(completePlayerList);
+        return completePlayerList;
+
+    }
+
+
+
+
+
+    //Helpmethods for tests below
+
 
     //method for test
     public List<String> getPlayerNames() {

142c665ec4c2150d22431ec691ce3cc06987e416
Viktoria Enderstein <vikend@student.chalmers.se>
2018-10-03 11:50:12 +0200

    created finishPageActivity class and xml

---
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/finishPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.drinkit.drinkit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class finishPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_page);
+    }
+}

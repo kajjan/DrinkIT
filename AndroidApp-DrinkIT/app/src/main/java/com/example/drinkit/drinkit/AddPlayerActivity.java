4a9150e54c4f493d5054a61279831e8e04ab2999
Viktoria Enderstein <vikend@student.chalmers.se>
2018-10-10 10:48:41 +0200

    added so that an error message appears if the same name is added more than once. The error message is shown on the textfields in question.

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,6 @@ import java.util.List;
 
 
 public class AddPlayerActivity extends MainView {
-
 
     EditText playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen;
     List<String> players = new ArrayList<>(10);
@@ -42,53 +42,108 @@ public class AddPlayerActivity extends MainView {
         playerNine= (EditText) findViewById(R.id.player9);
         playerTen = (EditText) findViewById(R.id.player10);
 
-        players.add(playerOne.getText().toString());
-        players.add(playerTwo.getText().toString());
-        players.add(playerThree.getText().toString());
-        players.add(playerFour.getText().toString());
-        players.add(playerFive.getText().toString());
-        players.add(playerSix.getText().toString());
-        players.add(playerSeven.getText().toString());
-        players.add(playerEight.getText().toString());
-        players.add(playerNine.getText().toString());
-        players.add(playerTen.getText().toString());
+        players.add(playerOne.getText().toString().trim());
+        players.add(playerTwo.getText().toString().trim());
+        players.add(playerThree.getText().toString().trim());
+        players.add(playerFour.getText().toString().trim());
+        players.add(playerFive.getText().toString().trim());
+        players.add(playerSix.getText().toString().trim());
+        players.add(playerSeven.getText().toString().trim());
+        players.add(playerEight.getText().toString().trim());
+        players.add(playerNine.getText().toString().trim());
+        players.add(playerTen.getText().toString().trim());
 
+        int numberOfSameNames=0;
 
-        for (int i = 0; i <players.size() ; i++) {
+         for (int i = 0; i <players.size() ; i++) {
             if (players.get(i).isEmpty()){
                 System.out.println("Nothing in this place of index"); //We dont need this later on, just for now
             }
             else{
                 System.out.println(players.get(i));
-                if (sameName(players, players.get(i))){
-                    startActivity(new Intent(AddPlayerActivity.this, ErrorMessageActivity.class));
+                if (sameName(players, players.get(i),i)){
                     System.out.println("Type in names again");
-                    return;
+                    numberOfSameNames++;
                 }
                 else{
                     getCtrl().addPlayer(players.get(i));
-                    startActivity(new Intent(AddPlayerActivity.this, ChooseCategoryActivity.class));
+                    }
                 }
+
             }
+            if(numberOfSameNames<1) {
+                startActivity(new Intent(AddPlayerActivity.this, ChooseCategoryActivity.class));
+            }
+
         }
-    }
+
+
+
+
+
 
     //Kan denna ersätts av en .contains??
-    public boolean sameName(List<String> players, String player){ //Method that checks if player already exists in list
-        int numberOfTimes=0;
-        for (String name: players) {
-            if(name.equals(player)){
-                numberOfTimes++;
+    public boolean sameName(List<String> players, String player, int index) { //Method that checks if player already exists in list
+        int numberOfTimes = 0;
+        int i=0;
+        for (String name : players) {
+                if(name.equals(player)){
+                    printErrorIfSameName(i, index);
+                    numberOfTimes++;
+                }
 
+                if (numberOfTimes > 1) {
+                    return true;
+                }
+                i++;
             }
-            if(numberOfTimes>1){
-                return true;
-            }
-        }
         return false;
+
     }
 
 
+//A method that send an error message too the textfield that has the same input as an other.
+    public void printErrorIfSameName(int i, int index){
+
+        if(i!=index) {
+            if (index == 0 | i == 0) {
+                playerOne.setError("Can't type in same name twice");
+            }
+            if (index == 1 | i == 1) {
+                playerTwo.setError("Can't type in same name twice");
+            }
+            if (index == 2 | i == 2) {
+                playerThree.setError("Can't type in same name twice");
+            }
+            if (index == 3 | i == 3) {
+                playerFour.setError("Can't type in same name twice");
+            }
+            if (index == 4 | i == 4) {
+                playerFive.setError("Can't type in same name twice");
+            }
+            if (index == 5 | i == 5) {
+                playerSix.setError("Can't type in same name twice");
+            }
+            if (index == 6 | i == 6) {
+                playerSeven.setError("Can't type in same name twice");
+            }
+            if (index == 7 | i == 7) {
+                playerEight.setError("Can't type in same name twice");
+            }
+            if (index == 8 | i == 8) {
+                playerNine.setError("Can't type in same name twice");
+            }
+            if (index == 9 | i == 9) {
+                playerTen.setError("Can't type in same name twice");
+            }
+        }
+
+    }
 
 
 }
+
+
+
+
+

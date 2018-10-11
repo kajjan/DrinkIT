0a70877865e43b51b304ada1e16bfdef53a8e8ee
Viktoria Enderstein <vikend@student.chalmers.se>
2018-10-11 20:06:03 +0200

    change the majority off the addPlayerActivity class so the class uses an observer so that ther errormessages comes during the input from the user

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/AddPlayerActivity.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import java.util.ArrayList;
@@ -16,20 +19,18 @@ import java.util.List;
 
 
 
-public class AddPlayerActivity extends MainView {
+public class AddPlayerActivity extends MainView implements TextWatcher{
 
     EditText playerOne, playerTwo, playerThree, playerFour, playerFive, playerSix, playerSeven, playerEight, playerNine, playerTen;
     List<String> players = new ArrayList<>(10);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
-    }
 
-
-    public void namesSubmitClick(View view) { //Method that saves all of the players and send it to the controller
-        players.clear();
 
         playerOne = (EditText) findViewById(R.id.player1);
         playerTwo = (EditText) findViewById(R.id.player2);
@@ -39,131 +40,199 @@ public class AddPlayerActivity extends MainView {
         playerSix = (EditText) findViewById(R.id.player6);
         playerSeven = (EditText) findViewById(R.id.player7);
         playerEight = (EditText) findViewById(R.id.player8);
-        playerNine= (EditText) findViewById(R.id.player9);
+        playerNine = (EditText) findViewById(R.id.player9);
         playerTen = (EditText) findViewById(R.id.player10);
 
-        players.add(playerOne.getText().toString().trim());
-        players.add(playerTwo.getText().toString().trim());
-        players.add(playerThree.getText().toString().trim());
-        players.add(playerFour.getText().toString().trim());
-        players.add(playerFive.getText().toString().trim());
-        players.add(playerSix.getText().toString().trim());
-        players.add(playerSeven.getText().toString().trim());
-        players.add(playerEight.getText().toString().trim());
-        players.add(playerNine.getText().toString().trim());
-        players.add(playerTen.getText().toString().trim());
+        playerOne.addTextChangedListener(this);
+        playerTwo.addTextChangedListener(this);
+        playerThree.addTextChangedListener(this);
+        playerFour.addTextChangedListener(this);
+        playerFive.addTextChangedListener(this);
+        playerSix.addTextChangedListener(this);
+        playerSeven.addTextChangedListener(this);
+        playerEight.addTextChangedListener(this);
+        playerNine.addTextChangedListener(this);
+        playerTen.addTextChangedListener(this);
 
-        int numberOfSameNames=0;
+
+    }
+
+
+    public void namesSubmitClick(View view) { //Method that saves all of the players and send it to the controller
+        players.clear();
         int numberOfNamesAdded=0;
 
-         for (int i = 0; i <players.size() ; i++) {
-            if (players.get(i).isEmpty()){
-                System.out.println("Nothing in this place of index"); //We dont need this later on, just for now
+        for (int i = 0; i <players.size() ; i++) {
+            if(players.get(i).isEmpty()){
             }
-            else{
-                System.out.println(players.get(i));
-                if (sameName(players, players.get(i),i)){
-                    System.out.println("Type in names again");
-                    numberOfSameNames++;
 
+            else{
+                getCtrl().addPlayer(players.get(i));
+                numberOfNamesAdded++;
+            }
+
+        }
+
+
+        if(numberOfNamesAdded<2){
+            atLeastTwoPlayersErrorMessage();
+        }
+
+    }
+
+
+
+
+    public void atLeastTwoPlayersErrorMessage(){
+        playerTwo.setError("At least two people must be added");
+    }
+
+
+
+
+
+
+
+
+
+    public int sameName(String name){
+        int numberOfTimes=0;
+        for (int i = 0; i < players.size(); i++) {
+            if(name.equals(players.get(i))){
+                numberOfTimes++;
+            }
+        }
+        return numberOfTimes;
+    }
+
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        Toast.makeText(this, "before changes", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Toast.makeText(this, "during chances", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        Toast.makeText(this, "after changes", Toast.LENGTH_SHORT).show();
+        String name= s.toString().trim();
+        if(playerOne.getText().hashCode() == s.hashCode()){
+            name=playerOne.getText().toString().trim();
+
+                if (sameName(name) > 1) {
+                    playerOne.setError("Can't type in same name twice");
                 }
                 else{
-                    getCtrl().addPlayer(players.get(i));
-                    }
-                numberOfNamesAdded++;
-
-            }
-
-            }
-            if(numberOfNamesAdded<=1){
-             atLeastTwoPlayersErrorMessage();
-            }
-            if(numberOfSameNames<1 & numberOfNamesAdded>1) {
-                startActivity(new Intent(AddPlayerActivity.this, ChooseCategoryActivity.class));
-            }
+                    players.add(name);
+                }
 
         }
 
+        if(playerTwo.getText().hashCode() == s.hashCode()){
+            name=playerTwo.getText().toString().trim();
 
 
-
-
-
-    //Kan denna ersätts av en .contains??
-    public boolean sameName(List<String> players, String player, int index) { //Method that checks if player already exists in list
-        int numberOfTimes = 0;
-        int i=0;
-        for (String name : players) {
-                if(name.equals(player)){
-                    printErrorIfSameName(i, index);
-                    numberOfTimes++;
+                if(sameName(name)>1){
+                    playerTwo.setError("Can't type in same name twice");
                 }
-
-                if (numberOfTimes > 1) {
-                    return true;
+                else{
+                    players.add(name);
                 }
-                i++;
-            }
-        return false;
+        }
 
-    }
+        if(playerThree.getText().hashCode() == s.hashCode()){
+            name=playerThree.getText().toString().trim();
 
 
-//A method that send an error message too the textfield that has the same input as an other.
-    public void printErrorIfSameName(int i, int index){
+                if (sameName(name) > 1) {
+                    playerThree.setError("Can't type in same name twice");
+                }
+                else{
+                    players.add(name);
+                }
+        }
 
-        if(i!=index) {
-            if (index == 0 | i == 0) {
-                playerOne.setError("Can't type in same name twice");
-            }
-            if (index == 1 | i == 1) {
-                playerTwo.setError("Can't type in same name twice");
-            }
-            if (index == 2 | i == 2) {
-                playerThree.setError("Can't type in same name twice");
-            }
-            if (index == 3 | i == 3) {
-                playerFour.setError("Can't type in same name twice");
-            }
-            if (index == 4 | i == 4) {
+        if(playerFour.getText().hashCode() == s.hashCode()){
+            name=playerFour.getText().toString().trim();
+
+
+                if (sameName(name) > 1) {
+                    playerFour.setError("Can't type in same name twice");
+                }
+                else{
+                    players.add(name);
+                }
+        }
+
+        if(playerFive.getText().hashCode() == s.hashCode()){
+            name=playerFive.getText().toString().trim();
+
+            if(sameName(name)>1){
                 playerFive.setError("Can't type in same name twice");
             }
-            if (index == 5 | i == 5) {
-                playerSix.setError("Can't type in same name twice");
-            }
-            if (index == 6 | i == 6) {
-                playerSeven.setError("Can't type in same name twice");
-            }
-            if (index == 7 | i == 7) {
-                playerEight.setError("Can't type in same name twice");
-            }
-            if (index == 8 | i == 8) {
-                playerNine.setError("Can't type in same name twice");
-            }
-            if (index == 9 | i == 9) {
-                playerTen.setError("Can't type in same name twice");
+            else{
+                players.add(name);
             }
         }
 
+        if(playerSix.getText().hashCode() == s.hashCode()){
+            name=playerSix.getText().toString().trim();
+
+            if(sameName(name)>1){
+                playerSix.setError("Can't type in same name twice");
+            }
+            else{
+                players.add(name);
+            }
+        }
+
+        if(playerSeven.getText().hashCode() == s.hashCode()){
+            name=playerSeven.getText().toString().trim();
+
+            if(sameName(name)>1){
+                playerSeven.setError("Can't type in same name twice");
+            }
+            else{
+                players.add(name);
+            }
+        }
+
+        if(playerEight.getText().hashCode() == s.hashCode()){
+            name=playerEight.getText().toString().trim();
+
+            if(sameName(name)>1){
+                playerEight.setError("Can't type in same name twice");
+            }
+            else{
+                players.add(name);
+            }
+        }
+
+        if(playerNine.getText().hashCode() == s.hashCode()){
+            name=playerNine.getText().toString().trim();
+
+            if(sameName(name)>1){
+                playerNine.setError("Can't type in same name twice");
+            }
+            else{
+                players.add(name);
+            }
+        }
+
+        if(playerTen.getText().hashCode() == s.hashCode()){
+            name=playerTen.getText().toString().trim();
+
+            if(sameName(name)>1){
+                playerTen.setError("Can't type in same name twice");
+            }
+            else{
+                players.add(name);
+            }
+        }
     }
-
-//A method that sends error messages than two players.
-   public void atLeastTwoPlayersErrorMessage(){
-       playerOne.setError("Need too at least add two players");
-       playerTwo.setError("Need too at least add two players");
-       playerThree.setError("Need too at least add two players");
-       playerFour.setError("Need too at least add two players");
-       playerFive.setError("Need too at least add two players");
-       playerSix.setError("Need too at least add two players");
-       playerSeven.setError("Need too at least add two players");
-       playerEight.setError("Need too at least add two players");
-       playerNine.setError("Need too at least add two players");
-       playerTen.setError("Need too at least add two players");
-   }
-
 }
-
-
-
-
-

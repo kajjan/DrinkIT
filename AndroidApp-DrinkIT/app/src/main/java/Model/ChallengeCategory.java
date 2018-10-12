d886c5575f276a4c5cbee679b983271f8995ddff
Viktoria Enderstein <vikend@student.chalmers.se>
2018-10-12 14:37:17 +0200

    small edits in the ChallengeFactory class

--- AndroidApp-DrinkIT/app/src/main/java/Model/ChallengeCategory.java
+++ AndroidApp-DrinkIT/app/src/main/java/Model/ChallengeCategory.java
@@ -1,4 +1,13 @@
 package Model;
 
+import java.util.List;
+
 public class ChallengeCategory {
+
+    public static Challenge createChallenge(){
+        /*switch (){
+
+        }*/
+        return null;
+    }
 }

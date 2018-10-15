6808c2aa3b3c105fdefc18ae5e0e2ca7ecabc128
Elin Eriksson <elinerikssonn@gmail.com>
2018-10-15 15:22:19 +0200

    created the pages for challenges with answers that appear hidden on another page. Two new views with corresponding xml files and new methods. They don't currently connect to the rest of the code completely, but work on their own

---
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChallengeWithAnswerPageOneActivity.java
@@ -0,0 +1,51 @@
+package com.example.drinkit.drinkit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ChallengeWithAnswerPageOneActivity extends MainView {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_with_answer);
+        getCtrl().createCompleteListOfPlayers();
+
+        printPlayersName();
+        printChallenge();
+    }
+
+    public void printPlayersName(){
+        TextView text=((TextView)findViewById(R.id.playerOfchallenge));
+        text.setText(getCtrl().getPlayersName());
+    }
+
+    public void printChallenge(){
+        TextView text=((TextView)findViewById(R.id.challengeText));
+        text.setText(getCtrl().getActiveChallenge());
+    }
+
+
+    public void toAnswerPage(View view) {
+        //getCtrl().failedChallenge();  //behövs annan metod?
+        toAnswerPageActivity();
+
+    }
+
+    public boolean nextRound(){
+        return getCtrl().nextRound();
+    }
+
+
+    public void changePage(View view) {
+        startActivity(new Intent(ChallengeWithAnswerPageOneActivity.this, FinishPageActivity.class));
+    }
+
+    public void toAnswerPageActivity(){
+        startActivity(new Intent(ChallengeWithAnswerPageOneActivity.this, ChallengeWithAnswerPageTwoActivity.class));
+
+    }
+
+}
--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/DurationActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/DurationActivity.java
@@ -1,13 +1,8 @@
 package com.example.drinkit.drinkit;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
-import Model.ChallengeWithAnswer;
-import Model.ChallengeWithPoints;
-import Model.ChallengeWithoutPoint;
 
 
 public class DurationActivity extends MainView {
@@ -16,7 +11,6 @@ public class DurationActivity extends MainView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duration);
-
     }
 
 
@@ -39,15 +33,15 @@ public class DurationActivity extends MainView {
     }
 
     public void startNextActivity(String category){
-        if (category.equals("quiz") || category.equals("song") || category.equals("charades") ){
+        if (category.equals("quiz") || category.equals("songs") || category.equals("charades") ){
             //Dubbelvy med poäng
-            startActivity(new Intent(DurationActivity.this, ChallengeWithAnswerActivity.class));
+            startActivity(new Intent(DurationActivity.this, ChallengeWithAnswerPageOneActivity.class));
         }
         else if(category.equals("truthOrDare")){
             //Enkelvy med poäng
             startActivity(new Intent(DurationActivity.this, ChallengeWithPointActivity.class));
         }
-        else if(category.equals("mostLikelyTo") || category.equals("rules")  || category.equals("neverHaveIEver") || category.equals("theme") || category.equals("thisOrThat") )
+        else if(category.equals("mostLikelyTo") || category.equals("rules")  || category.equals("neverHaveIEver") || category.equals("themes") || category.equals("thisOrThat") )
             //en vy utan poäng (ingen spelar)
         startActivity(new Intent(DurationActivity.this, ChallengeWithoutPointActivity .class));
         else{

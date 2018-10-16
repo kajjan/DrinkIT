d4e94e00a78bda662476c00495f37264b559516c
aliceolsson <34097397+aliceolsson@users.noreply.github.com>
2018-10-16 09:22:08 +0200

    started with the challengeview for those without points

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChallengeWithoutPointActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChallengeWithoutPointActivity.java
@@ -3,6 +3,8 @@ package com.example.drinkit.drinkit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ChallengeWithoutPointActivity extends MainView {
 
@@ -10,7 +12,25 @@ public class ChallengeWithoutPointActivity extends MainView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_without_point);
+        getCtrl().createCompleteListOfPlayers();
+
+        printPlayersName();
+
+        printChallenge();
     }
+    public void printPlayersName(){
+        TextView text=((TextView)findViewById(R.id.textViewPlayerChallengeWithoutPoint));
+        text.setText(getCtrl().getPlayersName());
+    }
+
+    public void printChallenge(){
+        TextView text=((TextView)findViewById(R.id.textViewChallengeWithoutPoint));
+        text.setText(getCtrl().getActiveChallenge());
+    }
+
+
+
+
 
     public void startNextActivity(String category) {
         if (category.equals("quiz") || category.equals("song") || category.equals("charades")) {
@@ -27,4 +47,25 @@ public class ChallengeWithoutPointActivity extends MainView {
         }
 
     }
+
+    public void challengeInstructionPage(View view) {
+        startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeInstructionActivity.class));
+    }
+
+    public boolean nextRound(){
+        return getCtrl().nextRound();
+    }
+
+    public void nextButtonWithoutPlayer(View view) {
+
+
+    }
+
+    public void changePage(View view) {
+        startActivity(new Intent(ChallengeWithoutPointActivity.this, FinishPageActivity.class));
+    }
+
+
 }
+
+

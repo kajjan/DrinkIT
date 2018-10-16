b5b6d10a71f49b5e5ba63f94d679e34c3a559707
aliceolsson <34097397+aliceolsson@users.noreply.github.com>
2018-10-16 09:29:21 +0200

    fixed a bit more with the challengeview for those without points

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChallengeWithoutPointActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChallengeWithoutPointActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import Model.ChallengeWithoutPoint;
+
 public class ChallengeWithoutPointActivity extends MainView {
 
     @Override
@@ -29,41 +31,39 @@ public class ChallengeWithoutPointActivity extends MainView {
 
 
 
+    public void nextButtonWithoutPlayer(View view) {
+        changePage(view);
+        String nextCategory = getCtrl().getNextCategory();
+        startNextActivity(nextCategory);
+
+    }
 
 
-    public void startNextActivity(String category) {
-        if (category.equals("quiz") || category.equals("song") || category.equals("charades")) {
-            //Dubbelvy med poäng
-            startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeWithAnswerPageOneActivity.class));
-        } else if (category.equals("truthOrDare")) {
-            //Enkelvy med poäng
-            startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeWithPointActivity.class));
-        } else if (category.equals("mostLikelyTo") || category.equals("rules") || category.equals("neverHaveIEver") || category.equals("theme") || category.equals("thisOrThat"))
-            //en vy utan poäng (ingen spelar)
-            startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeWithoutPointActivity.class));
-        else {
-            System.out.println("Something is wrong with the code in DurationActivity...");
-        }
-
+    public void changePage(View view) {
+        startActivity(new Intent(ChallengeWithoutPointActivity.this, FinishPageActivity.class));
     }
 
     public void challengeInstructionPage(View view) {
         startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeInstructionActivity.class));
     }
 
-    public boolean nextRound(){
-        return getCtrl().nextRound();
+    public void startNextActivity(String category){
+        if (category.equals("quiz") || category.equals("song") || category.equals("charades") ){
+            //Dubbelvy med poäng
+            startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeWithAnswerPageOneActivity.class));
+        }
+        else if(category.equals("truthOrDare")){
+            //Enkelvy med poäng
+            startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeWithPointActivity.class));
+        }
+        else if(category.equals("mostLikelyTo") || category.equals("rules")  || category.equals("neverHaveIEver") || category.equals("theme") || category.equals("thisOrThat") )
+            //en vy utan poäng (ingen spelar)
+            startActivity(new Intent(ChallengeWithoutPointActivity.this, ChallengeWithoutPointActivity .class));
+        else{
+            System.out.println("Something is wrong with the code in DurationActivity...");
+        }
+
     }
-
-    public void nextButtonWithoutPlayer(View view) {
-
-
-    }
-
-    public void changePage(View view) {
-        startActivity(new Intent(ChallengeWithoutPointActivity.this, FinishPageActivity.class));
-    }
-
 
 }
 

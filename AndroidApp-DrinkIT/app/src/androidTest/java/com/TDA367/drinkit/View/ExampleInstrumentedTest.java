41f95ac74d2b31ec9d8088f333677370940af736
kajjan <bjarang@student.chalmers.se>
2018-10-17 19:38:38 +0200

    change path for test. trying to fix travis...

--- AndroidApp-DrinkIT/app/src/androidTest/java/com/TDA367/drinkit/View/ExampleInstrumentedTest.java
+++ AndroidApp-DrinkIT/app/src/androidTest/java/com/TDA367/drinkit/View/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.drinkit.drinkit", appContext.getPackageName());
+        assertEquals("com.TDA367.drinkit.View", appContext.getPackageName());
     }
 }

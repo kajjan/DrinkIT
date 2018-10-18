4fc2d935ded888ad51c8b95e5f3c93dc2141c2cc
aliceolsson <34097397+aliceolsson@users.noreply.github.com>
2018-10-18 11:05:07 +0200

    deleted unecessary code

--- AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChooseCategoryActivity.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/example/drinkit/drinkit/ChooseCategoryActivity.java
@@ -3,9 +3,6 @@ package com.example.drinkit.drinkit;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.RippleDrawable;
 import android.os.Build;
 import android.os.Bundle;
 

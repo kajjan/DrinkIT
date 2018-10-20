d6ea7e9e8f001e612c9c02b64407123c847e8e58
Alice Olsson <alice_olsson@hotmail.com>
2018-10-20 17:35:51 +0200

    Truth or dare activity add

--- AndroidApp-DrinkIT/app/src/main/java/com/TDA367/drinkit/View/ChallengeDecorator.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/TDA367/drinkit/View/ChallengeDecorator.java
@@ -1,14 +1,22 @@
 package com.TDA367.drinkit.View;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public abstract class ChallengeDecorator implements Decorator {
+public abstract class ChallengeDecorator extends AppCompatActivity implements Decorator {
+
+   public ChallengeActivity challengeActivity;
 
 
-    public ChallengeDecorator(){
+    public ChallengeDecorator(ChallengeActivity ca){
         super();
+        this.challengeActivity = ca;
         decorate();
     }
 
+
+    public ChallengeDecorator() {
+
+    }
 }
--- AndroidApp-DrinkIT/app/src/main/java/com/TDA367/drinkit/View/ChallengeTruthOrDare.java
+++ AndroidApp-DrinkIT/app/src/main/java/com/TDA367/drinkit/View/ChallengeTruthOrDare.java
@@ -1,21 +1,43 @@
 package com.TDA367.drinkit.View;
 
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class ChallengeTruthOrDare extends ChallengeDecorator {
 
-    protected RelativeLayout challengeTruthOrDare;
+  static ChallengeActivity challengeActivity;
 
-
-    public ChallengeTruthOrDare(RelativeLayout challengeTruthOrDare) {
-        super();
-        this.challengeTruthOrDare=challengeTruthOrDare;
+    public ChallengeTruthOrDare() {
+        super(challengeActivity);
     }
+
+
+        public void hereAreButtons() {
+            RelativeLayout truthOrDare = (RelativeLayout) findViewById(R.id.challenge_base);
+            Button dareButton = new Button(this);
+            dareButton.setText("Dare");
+            dareButton.setTextSize(25);
+            LinearLayout.LayoutParams layoutParamsDare = new LinearLayout.LayoutParams(380, 180);
+            layoutParamsDare.setMargins(600, 1100, 0, 0); // left, top, right, bottom
+            dareButton.setLayoutParams(layoutParamsDare);
+            truthOrDare.addView(dareButton);
+
+            Button truthButton = new Button(this);
+            truthButton.setText("Truth");
+            truthButton.setTextSize(25);
+            LinearLayout.LayoutParams layoutParamsTruth = new LinearLayout.LayoutParams(380, 180);
+            layoutParamsTruth.setMargins(100, 1100, 0, 0); //left, top, right, bottom
+            truthButton.setLayoutParams(layoutParamsTruth);
+            truthOrDare.addView(truthButton);
+        }
 
 
     @Override
     public void decorate() {
-        challengeTruthOrDare.setVisibility(challengeTruthOrDare.VISIBLE);
+        // challengeTruthOrDare.setVisibility(challengeTruthOrDare.VISIBLE);
+        hereAreButtons();
+
     }
 
 }

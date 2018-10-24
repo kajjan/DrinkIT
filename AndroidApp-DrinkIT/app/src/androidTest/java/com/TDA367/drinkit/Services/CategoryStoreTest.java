cd927854ae075161698d747565a5f693555f7477
Elin Eriksson <elinerikssonn@gmail.com>
2018-10-24 14:21:01 +0200

    fixed problem in CategoryStoreTest

--- AndroidApp-DrinkIT/app/src/androidTest/java/com/TDA367/drinkit/Services/CategoryStoreTest.java
+++ AndroidApp-DrinkIT/app/src/androidTest/java/com/TDA367/drinkit/Services/CategoryStoreTest.java
@@ -1,5 +1,8 @@
 package com.TDA367.drinkit.Services;
 
+import com.TDA367.drinkit.Controller.Controller;
+import com.TDA367.drinkit.Model.DrinkIT;
+
 import org.json.JSONObject;
 import org.junit.Test;
 import java.io.FileInputStream;
@@ -8,10 +11,13 @@ import java.io.InputStream;
 
 public class CategoryStoreTest {
 
+    //is it correct to add these here?
+    DrinkIT model = new DrinkIT();
+    Controller ctrl = new Controller(model);
 
     @Test
     public void jsonTest() {
-        CategoryStore catstore = new CategoryStore();
+        CategoryStore catstore = new CategoryStore(ctrl);
         String file = "/Users/elineriksson/Documents/Github/DrinkIT/AndroidApp-DrinkIT/app/src/main/assets/charades.json";
 
         try {
